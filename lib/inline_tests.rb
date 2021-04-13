d93766d7e8dbbc70193f7ba288a17312274eb6ad
Andrew Brown <drusepth@gmail.com>
2021-04-13 01:25:19 -0700

    don't use scientific notation for tiny time values

--- lib/inline_tests.rb
+++ lib/inline_tests.rb
@@ -18,14 +18,14 @@ class InlineTests
         test_time    = Time.now - start_time
         test_passes += 1
 
-        puts "  #{method_signature} - PASSED (#{test_time} seconds)"
+        puts "  #{method_signature} - PASSED (#{format_tiny_time test_time} seconds)"
 
       rescue InlineTestFailure => failure_information
         test_time   = Time.now - start_time
         errors      = failure_information
         test_fails += 1
 
-        puts "  #{method_signature} - FAILED (#{test_time} seconds)"
+        puts "  #{method_signature} - FAILED (#{format_tiny_time test_time} seconds)"
         puts errors
       end
     end
@@ -33,8 +33,14 @@ class InlineTests
 
     # Print overall test results & stats
     # print_results(method_passing, method_errors)
-    puts "#{tested_methods.count} inline tests ran in #{all_tests_end_time - all_tests_start_time} seconds."
+    puts "#{tested_methods.count} inline tests ran in #{format_tiny_time all_tests_end_time - all_tests_start_time} seconds."
     puts "  #{test_passes} PASSED"
     puts "  #{test_fails} FAILS"
+  end
+
+  private
+
+  def self.format_tiny_time(time)
+    sprintf('%.15f', time).sub(/0+$/, '').sub(/\.$/, '.0')
   end
 end
